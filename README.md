b2f5a2a59eac0243d74aad2dab3f8539f65a8dbd
ZenQy <zenqy.qin@gmail.com>
2021-10-08 11:57:18 +0800

    添加aarch64架构配置

---
+++ README.md
@@ -0,0 +1,48 @@
+# OpenWrt
+
+[![lede](https://img.shields.io/badge/github-lede-blue.svg?style=flat&logo=github)](https://github.com/coolsnowwolf/lede)
+![GitHub](https://img.shields.io/github/license/ZenQy/Openwrt)
+
+## 如何使用
+
+1. fork项目
+2. 在secrets中创建`RELEASES_TOKEN`，一般一次编译要2~4小时，所以要创建一个github发布用的token
+3. 在secrets中创建`TELEGRAM_TO`和`TELEGRAM_TOKEN`，用户通知
+4. 点击Actions -> Workflows -> Run workflow -> Run workflow 
+5. Build Flippy OpenWrt Package 编译n1等盒子可用包
+
+------
+
+## 用户名和密码
+
+ * User: root
+ * Password: password
+ * Default IP: 192.168.1.5
+
+------
+
+## 全新刷入emmc方法
+
+  1. 固件刷入U盘。
+  2. openwrt-install-amlogic
+  3. 拔掉U盘，断电重启
+
+------
+
+## 在线升级方法
+
+  1. 上传 img 到/mnt/mmcblk2p4
+  2. cd /mnt/mmcblk2p4
+  3. gzip -d  *.img.gz
+  4. openwrt-update-amlogic xxxx.img
+
+------
+
+## 感激 
+
+ - [暴躁老哥](https://github.com/breakings/OpenWrt)
+ - [mingxiaoyu](https://github.com/mingxiaoyu/N1Openwrt)
+
+## License
+
+[MIT](https://github.com/ZenQy/Openwrt/blob/main/LICENSE)
---
+++ diy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+
+echo '修改机器名称'
+sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+
+# Add OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash
+
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+rm -rf ../lean/luci-theme-argon
+
+# Add v2ray
+git clone --depth=1 -b v1.5.6-1 https://github.com/kuoruan/luci-app-v2ray
+git clone --depth=1 https://github.com/kuoruan/openwrt-v2ray
+
+popd
+
+# 删除冗余软件
+rm -rf package/lean/UnblockNeteaseMusic*
+rm -rf package/lean/luci-app-unblockmusic
+
+# 替换默认主题
+sed -i 's#luci-theme-bootstrap#luci-theme-argon#g' feeds/luci/collections/luci/Makefile
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# Openwrt version
+version=$(grep "DISTRIB_REVISION=" package/lean/default-settings/files/zzz-default-settings  | awk -F "'" '{print $2}')
+sed -i '/DISTRIB_REVISION/d' package/lean/default-settings/files/zzz-default-settings
+echo "echo \"DISTRIB_REVISION='${version} $(TZ=UTC-8 date "+%Y.%m.%d") Compilde by ZenQy'\" >> /etc/openwrt_release" >> package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit 0/d' package/lean/default-settings/files/zzz-default-settings
+echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
