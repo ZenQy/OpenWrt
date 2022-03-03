6f1762cf398a532fe9dd95350bc0534d952124cc
ZenQy <zenqy.qin@gmail.com>
2022-03-03 19:55:58 +0800

    update

--- diy.sh
+++ diy.sh
@@ -10,7 +10,7 @@
 # sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.11/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.5/g' package/base-files/files/bin/config_generate
 
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
@@ -38,10 +38,13 @@ git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 popd
 
 # 替换默认主题
-sed -i 's#luci-theme-bootstrap#luci-theme-argon#g' feeds/luci/collections/luci/Makefile
-ls feeds/luci/themes
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 rm -rf feeds/luci/themes
-ls feeds/luci/themes
+
+# 删除部分DEFAULT_PACKAGES
+
+sed -i 's/luci-app-ssr-plus //g' include/target.mk
+sed -i 's/luci-app-unblockmusic //g' include/target.mk
 
 # Openwrt version
 version=$(grep "DISTRIB_REVISION=" package/lean/default-settings/files/zzz-default-settings  | awk -F "'" '{print $2}')
