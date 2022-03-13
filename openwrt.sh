9d6ea88985cf2a71eec89a422f5e179be5fdb52c
ZenQy <zenqy.qin@gmail.com>
2022-03-13 18:04:02 +0800

    移除xray

--- openwrt.sh
+++ openwrt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #=================================================
 # Description: DIY script
 # Lisence: MIT
@@ -6,14 +7,25 @@
 # Blog: https://p3terx.com
 #=================================================
 
+# 修改config_generate
+
+echo '修改默认IP'
+sed -i 's/192.168.1.1/10.0.0.11/g' package/base-files/files/bin/config_generate
+
+echo '默认时区'
+sed -i "s/'UTC'/'CST-8'\n\t\tset system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+echo 'NTP服务器'
+sed -i 's/0.openwrt.pool.ntp.org/ntp.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/time.ustc.edu.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/cn.pool.ntp.org/g' package/base-files/files/bin/config_generate
+
 # echo '修改机器名称'
 # sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
 
-# Modify default IP
-sed -i 's/192.168.1.1/10.0.0.11/g' package/base-files/files/bin/config_generate
-
-echo '修改时区'
-sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+echo '设置默认语言'
+sed -i "1a \nuci set luci.main.lang=zh_cn\nuci commit luci" package/base-files/files/bin/config_generate 
 
 # Clone community packages to package/community
 mkdir package/community
@@ -21,9 +33,6 @@ pushd package/community
 
 # Add OpenClash
 git clone --depth=1 https://github.com/vernesong/OpenClash
-
-# Add xray
-git clone --depth=1 https://github.com/yichya/luci-app-xray
 
 # Add theme
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
