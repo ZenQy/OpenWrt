e707b026d8631fad1a7fcc300eabbda8a768cf5e
ZenQy <zenqy.qin@gmail.com>
2023-02-22 21:53:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -34,11 +34,11 @@ ls | grep -v adguardhome \
 cd ../../base-files/files
 mkdir -p etc/openclash/core
 CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/core-lateset/premium | grep download_url | grep arm64 | awk -F '"' '{print $4}')
-GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
-GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+# GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+# GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 wget -qO- $CLASH_TUN_URL | gunzip -c > etc/openclash/core/clash_tun
-wget -qO- $GEOIP_URL > etc/openclash/GeoIP.dat
-wget -qO- $GEOSITE_URL > etc/openclash/GeoSite.dat
+# wget -qO- $GEOIP_URL > etc/openclash/GeoIP.dat
+# wget -qO- $GEOSITE_URL > etc/openclash/GeoSite.dat
 chmod +x etc/openclash/core/clash*
 # authorized_keys
 mkdir -p etc/dropbear
